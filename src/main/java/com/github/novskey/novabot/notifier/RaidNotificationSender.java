28356672a86e7db913b13dd55baddb795716b31b
Jerry <JerryLosey@users.noreply.github.com>
2018-02-22 15:10:14 -0600

    Permission error handling

--- src/main/java/com/github/novskey/novabot/notifier/RaidNotificationSender.java
+++ src/main/java/com/github/novskey/novabot/notifier/RaidNotificationSender.java
@@ -6,9 +6,12 @@ import com.github.novskey.novabot.core.NovaBot;
 import com.github.novskey.novabot.maps.GeofenceIdentifier;
 import com.github.novskey.novabot.raids.RaidLobby;
 import com.github.novskey.novabot.raids.RaidSpawn;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
+import net.dv8tion.jda.core.exceptions.PermissionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -147,22 +150,61 @@ public class RaidNotificationSender extends NotificationSender implements Runnab
             localLog.info(String.format("Checking supporter status of %s", user.getName()));
             novaBot.lastUserRoleChecks.put(userID, currentTime);
             if (checkSupporterStatus(user)) {
-                user.openPrivateChannel().queue(channel -> channel.sendMessage(message).queue(
-                        msg -> {
-                            if (showTick) {
-                                msg.addReaction(WHITE_GREEN_CHECK).queue();
-                            }
-                        }, novaBot.notificationLogger.logPMBlocked(user)
-                ));
+                user.openPrivateChannel().queue(
+                        success -> success.sendMessage(message).queue(
+                                suc -> {
+                                    if (showTick) {
+                                        suc.addReaction(WHITE_GREEN_CHECK).queue();
+                                    }
+                                },
+                                err -> {
+                                    if (err instanceof PermissionException) {
+                                        PermissionException pe = (PermissionException) err;
+                                        Permission missingPermission = pe.getPermission();
+                                        novaBot.novabotLog.info("On sendMessage: PermissionError: " + err.getMessage() + " \n Missing permission: " + missingPermission.getName() + "\nUser: " + user.getName());
+                                    } else
+                                        novaBot.novabotLog.info("Unknown error sending message to user: " + user.getName() + "\n" + err.getMessage() + "\nUser: " + user.getName());
+                                }
+                        ),
+                        error -> {
+                            if (error instanceof PermissionException) {
+                                PermissionException pe = (PermissionException) error;
+                                Permission missingPermission = pe.getPermission();
+                                novaBot.novabotLog.info("On openPrivateChannel: PermissionError: " + error.getMessage() + " \n Missing permission: " + missingPermission.getName() + "\nUser: " + user.getName());
+                            } else
+                                novaBot.novabotLog.info("Unknown error opening private channel with user: " + user.getName() + "\n" + error.getMessage() + "\nUser: " + user.getName());
+                        }
+                );
             }
         } else {
-            user.openPrivateChannel().queue(channel -> channel.sendMessage(message).queue(
-                    msg -> {
-                        if (showTick) {
-                            msg.addReaction(WHITE_GREEN_CHECK).queue();
-                        }
-                    }, novaBot.notificationLogger.logPMBlocked(user)
-            ));
+            //Is not supporter:
+            user.openPrivateChannel().queue(
+
+                    success -> success.sendMessage(message).queue(
+
+                            suc -> {
+                                if (showTick) {
+                                    suc.addReaction(WHITE_GREEN_CHECK).queue();
+                                }
+                            },
+                            err -> {
+                                if (err instanceof PermissionException) {
+                                    PermissionException pe = (PermissionException) err;
+                                    Permission missingPermission = pe.getPermission();
+                                    novaBot.novabotLog.info("On sendMessage: PermissionError: " + err.getMessage() + " \n Missing permission: " + missingPermission.getName() + "\nUser: " + user.getName());
+                                } else
+                                    novaBot.novabotLog.info("Unknown error sending message to user: " + user.getName() + "\n" + err.getMessage() + "\nUser: " + user.getName());
+                            }
+                    ),
+                    error -> {
+                        if (error instanceof PermissionException) {
+                            PermissionException pe = (PermissionException) error;
+                            Permission missingPermission = pe.getPermission();
+                            novaBot.novabotLog.info("On openPrivateChannel: PermissionError: " + error.getMessage() + " \n Missing permission: " + missingPermission.getName() + "\nUser: " + user.getName());
+                        } else
+                            novaBot.novabotLog.info("Unknown error opening private channel with user: " + user.getName() + "\n" + error.getMessage() + "\nUser: " + user.getName());
+                    }
+            );
         }
     }
 
@@ -173,11 +215,32 @@ public class RaidNotificationSender extends NotificationSender implements Runnab
         if(channel == null){
             localLog.warn(String.format("Couldn't find from ID %s",channelId));
         }
-        channel.sendMessage(message).queue(m -> {
-            if (novaBot.getConfig().isRaidOrganisationEnabled() && raidLevel >= 3) {
-                System.out.println(String.format("adding reaction to raid with raidlevel %s", raidLevel));
-                m.addReaction(WHITE_GREEN_CHECK).queue();
-            }
-        });
+        channel.sendMessage(message).queue(
+                success -> {
+                    if (novaBot.getConfig().isRaidOrganisationEnabled() && raidLevel >= 3) {
+                        System.out.println(String.format("adding reaction to raid with raidlevel %s", raidLevel));
+                        success.addReaction(WHITE_GREEN_CHECK).queue(
+                                s -> {
+                                    localLog.info("Added reaction checkmark to message");
+                                },
+                                error -> {
+                                    if (error instanceof InsufficientPermissionException) {
+                                        InsufficientPermissionException pe = (InsufficientPermissionException) error;
+                                        Permission missingPermission = pe.getPermission();
+                                        novaBot.novabotLog.info("Channel: " + channel.getName() + " (" + channelId + ") PermissionError: " + ((PermissionException) error).getMessage() + " \n Missing permission: " + missingPermission.getName());
+                                    } else
+                                        novaBot.novabotLog.info("Unknown error sending message to channel: " + channelId + "\n" + error.getMessage());
+                                });
+                    }
+                },
+                error -> {
+                    if (error instanceof InsufficientPermissionException) {
+                        InsufficientPermissionException pe = (InsufficientPermissionException) error;
+                        Permission missingPermission = pe.getPermission();
+                        novaBot.novabotLog.info("Channel: " + channel.getName() + " (" + channelId + ") PermissionError: " + ((PermissionException) error).getMessage() + " \n Missing permission: " + missingPermission.getName());
+                    } else
+                        novaBot.novabotLog.info("Unknown error sending message to channel: " + channelId + "\n" + error.getMessage());
+                }
+        );
     }
 }
