345f4499ce288cbbea3eaf154b202f4f55727c11
Jerry <JerryLosey@users.noreply.github.com>
2018-02-22 15:10:42 -0600

    Permission error handling

--- src/main/java/com/github/novskey/novabot/notifier/NotificationSender.java
+++ src/main/java/com/github/novskey/novabot/notifier/NotificationSender.java
@@ -2,7 +2,9 @@ package com.github.novskey.novabot.notifier;
 
 import com.github.novskey.novabot.core.NotificationLimit;
 import com.github.novskey.novabot.core.NovaBot;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.exceptions.PermissionException;
 
 import java.util.function.Consumer;
 
@@ -45,8 +47,30 @@ class NotificationSender {
     private void resetUser(User user, NotificationLimit newLimit) {
         novaBot.dataManager.resetUser(user.getId());
 
-        user.openPrivateChannel().queue(channel -> channel.sendMessageFormat("Hi %s, I noticed that recently your supporter status has changed." +
-                " As a result I have cleared your settings. At your current level you can add up to %s to your settings.", user, newLimit.toWords()).queue(), novaBot.notificationLogger.logPMBlocked(user));
+        user.openPrivateChannel().queue(
+
+                success -> {
+                    success.sendMessageFormat("Hi %s, I noticed that recently your supporter status has changed. As a result I have cleared your settings. At your current level you can add up to %s to your settings.", user, newLimit.toWords()).queue(
+                            s -> novaBot.novabotLog.info("Successfully sent message to " + user.getName()),
+                            e -> {
+                                if (e instanceof PermissionException) {
+                                    PermissionException pe = (PermissionException) e;
+                                    Permission missingPermission = pe.getPermission();
+                                    novaBot.novabotLog.info("On openPrivateChannel: PermissionError: " + e.getMessage() + " \n Missing permission: " + missingPermission.getName() + "\nUser: " + user.getName());
+                                } else
+                                    novaBot.novabotLog.info("Unknown error opening private channel with user: " + user.getName() + "\n" + e.getMessage() + "\nUser: " + user.getName());
+                            }
+                    );
+                },
+                err -> {
+                    if (err instanceof PermissionException) {
+                        PermissionException pe = (PermissionException) err;
+                        Permission missingPermission = pe.getPermission();
+                        novaBot.novabotLog.info("On openPrivateChannel: PermissionError: " + err.getMessage() + " \n Missing permission: " + missingPermission.getName() + "\nUser: " + user.getName());
+                    } else
+                        novaBot.novabotLog.info("Unknown error opening private channel with user: " + user.getName() + "\n" + err.getMessage() + "\nUser: " + user.getName());
+                }
+        );
 
         if (novaBot.getConfig().loggingEnabled()) {
             novaBot.roleLog.sendMessageFormat("%s's supporter status has changed, requiring a reset of their settings. They have been informed via PM.", user).queue();
