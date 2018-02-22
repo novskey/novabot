a943338566de792381b3dde3c50b0fabc49bc51a
Jerry <JerryLosey@users.noreply.github.com>
2018-02-22 09:24:38 -0600

    Don't pause user because several other situations could cause the private channel send to fail. Simply log it and move on. Also adding a channel logger.

--- src/main/java/com/github/novskey/novabot/Util/NotificationLogger.java
+++ src/main/java/com/github/novskey/novabot/Util/NotificationLogger.java
@@ -2,6 +2,7 @@ package com.github.novskey.novabot.Util;
 
 import com.github.novskey.novabot.core.NovaBot;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.Channel;
 
 import java.util.function.Consumer;
 
@@ -13,8 +14,12 @@ public class NotificationLogger {
     }
 
     public Consumer<Throwable> logPMBlocked(User user) {
-        nova.novabotLog.info(user.getName() + " has messages blocked. Please inform them to pause notifications instead of blocking. I will now ensure user is paused.");
-        nova.dataManager.pauseUser(user.getId());
+        nova.novabotLog.info("Failed sending private message to user: " + user.getName() + ".  hasPMChannel=" + user.hasPrivateChannel());
+        return null;
+    }
+
+    public Consumer<Throwable> logBadChannelPerms(String channel) {
+        nova.novabotLog.info("Failed sending message to channel: " + channel);
         return null;
     }
 
